a8f08afc9441340b54bf522e51b644d31ef6b409
Jarrod Davis <developer@jarrodldavis.com>
2020-10-21 12:18:51 -0600

    extract tailwind utility classes

--- index.ts
+++ index.ts
@@ -1,1 +1,29 @@
 console.log("Hello, world!");
+
+import postcss, { Root, Transformer } from "postcss";
+import createSelectorParser from "postcss-selector-parser";
+import tailwindcss from "tailwindcss";
+
+async function extractClasses(): Promise<Set<string>> {
+  const classes = new Set<string>();
+
+  const transformer: Transformer = (root: Root): void => {
+    const classExtractor = createSelectorParser((selectors) =>
+      selectors.walkClasses((node) => {
+        classes.add(node.value);
+      })
+    );
+
+    root.walkRules((rule) => {
+      classExtractor.processSync(rule.selector);
+    });
+  };
+
+  const css = "@tailwind base; @tailwind utilities; @tailwind components;";
+
+  await postcss(tailwindcss, transformer).process(css, { from: undefined });
+
+  return classes;
+}
+
+extractClasses().then(console.log, console.error);
---
+++ tailwindcss.d.ts
@@ -0,0 +1,5 @@
+declare module "tailwindcss" {
+  import { AcceptedPlugin } from "postcss";
+  const tailwindcss: AcceptedPlugin;
+  export default tailwindcss;
+}
