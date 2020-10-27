6d7f4514e6d7e1491818438022a0dbc544584436
Jarrod Davis <developer@jarrodldavis.com>
2020-10-27 17:03:18 -0600

    allow nested fixtures

--- test/load-test-cases.ts
+++ test/load-test-cases.ts
@@ -8,27 +8,44 @@ interface TestCases {
   invalid: RuleTester.InvalidTestCase[];
 }
 
-function read(...segments: string[]): string {
-  return fs.readFileSync(path.join(...segments), { encoding: "utf-8" });
-}
-
 export default function loadTestCases(rule: string): TestCases {
   const fixtureRoot = path.join(__dirname, "fixtures", rule);
 
-  const valid = fs.readdirSync(path.join(fixtureRoot, "valid")).map((name) => ({
-    filename: path.basename(name, path.extname(name)),
-    code: read(fixtureRoot, "valid", name),
+  function* getFixtures(type: "valid" | "invalid", dir = ""): Generator<string> {
+    const fulldir = path.join(fixtureRoot, type, dir);
+    for (const entry of fs.readdirSync(fulldir, { withFileTypes: true })) {
+      const name = path.join(dir, entry.name);
+      if (entry.isFile()) {
+        const extname = path.extname(entry.name);
+        if (extname === ".jsx") {
+          yield name.slice(0, -extname.length);
+        } else if (type === "invalid" && extname === ".json") {
+          continue;
+        } else {
+          throw new Error(`Unexpected file '${name}'.`);
+        }
+      } else if (entry.isDirectory()) {
+        yield* getFixtures(type, name);
+      } else {
+        throw new Error(`Directory entry '${name}' is neither a file nor a directory.`);
+      }
+    }
+  }
+
+  function read(type: "valid" | "invalid", name: string, extname: string): string {
+    return fs.readFileSync(path.join(fixtureRoot, type, `${name}${extname}`), { encoding: "utf-8" });
+  }
+
+  const valid = Array.from(getFixtures("valid")).map((name) => ({
+    filename: name,
+    code: read("valid", name, ".jsx"),
   }));
 
-  const invalid = fs
-    .readdirSync(path.join(fixtureRoot, "invalid"))
-    .filter((name) => path.extname(name) === ".jsx")
-    .map((name) => path.basename(name, path.extname(name)))
-    .map((name) => ({
-      filename: name,
-      code: read(fixtureRoot, "invalid", `${name}.jsx`),
-      errors: JSON.parse(read(fixtureRoot, "invalid", `${name}.json`)) as RuleTester.TestCaseError[],
-    }));
+  const invalid = Array.from(getFixtures("invalid")).map((name) => ({
+    filename: name,
+    code: read("invalid", name, ".jsx"),
+    errors: JSON.parse(read("invalid", name, ".json")) as RuleTester.TestCaseError[],
+  }));
 
   return { valid, invalid };
 }
