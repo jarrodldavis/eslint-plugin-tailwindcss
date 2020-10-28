be2bddc521cdcd0fd63e783b0c41957fcaa388dc
Jarrod Davis <developer@jarrodldavis.com>
2020-10-28 11:56:59 -0600

    add more tests

---
+++ test/fixtures/no-unknown-class/invalid/attribute/expression/clsx/literals-string-multi.jsx
@@ -0,0 +1,5 @@
+import clsx from "clsx";
+
+export default function Component() {
+  return <div className={clsx("class-a class-b", "class-c class-d")}></div>;
+}
---
+++ test/fixtures/no-unknown-class/invalid/attribute/expression/logical/literal-string-multi.jsx
@@ -0,0 +1,3 @@
+export default function Component() {
+  return <div className={true && "class-a class-b"}></div>;
+}
